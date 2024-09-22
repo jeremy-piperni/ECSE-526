0464355b84b9f4b045c886ff9b052f3bdbf11367
jeremy-piperni <jeremypiperni@hotmail.com>
2024-09-22 12:16:56 -0400

    Added readme, finalized main.py

---
+++ A1/README.txt
@@ -0,0 +1,11 @@
+Variables that can be changed by the user:
+
+connect_to_server: True = program will connect to the game server for the chess tournament
+		   False = program will run in command line (see AI_vs_AI variable)
+game_id: game id for the chess tournament
+playing_color: wanted playing color of the AI
+TCP_IP: ip for chess tournament server
+TCP_PORT: port for chess tournament server
+AI_vs_AI: True = simulates a game of the AI playing white vs AI playing black
+	  False = makes the user be able to input moves and play against the AI
+depth: select depth of state expansion for the AI
--- A1/main.py
+++ A1/main.py
@@ -1,25 +1,26 @@
 import numpy as np
-import time
 import socket
 
-playing_board = np.array([['x','x','x','x','x','x','x','x'],['x','x','x','x','x','x','x','x'],['x','x','x','x','x','x','x','x'],['x','x','x','x','x','x','x','x'],
-                  ['x','x','x','x','x','x','x','x'],['x','x','x','x','x','x','x','x'],['x','x','x','x','x','x','x','x'],['x','x','x','x','x','x','x','x']])
-is_white = True
-depth = 3
-game_won = False
-states_visited = 0
+# Variables that can be changed by the user
+connect_to_server = True
 game_id = "12345"
 playing_color = "white"
 TCP_IP = "156trlinux-1.ece.mcgill.ca"
 TCP_PORT = 12345
+AI_vs_AI = True
+depth = 5
+
+# Variables that should not be changed by the user
+playing_board = np.array([['x','x','x','x','x','x','x','x'],['x','x','x','x','x','x','x','x'],['x','x','x','x','x','x','x','x'],['x','x','x','x','x','x','x','x'],
+                  ['x','x','x','x','x','x','x','x'],['x','x','x','x','x','x','x','x'],['x','x','x','x','x','x','x','x'],['x','x','x','x','x','x','x','x']])
+is_white = True
+game_won = False
 BUFFER_SIZE = 1024
 
-
+# get the valid moves on the board for a given color
 def get_valid_moves(board, color):
     valid_moves = []
-    for col in range(8):
-        if valid_row(board, col) != 0:
-            valid_moves.append("D " + str(col + 1))
+    drop_order = [3,4,2,5,1,6,0,7]
     if color == "white":
         runsOfThree = runs_of_three_hor(board, "white")
         runsOfTwo = runs_of_two_hor(board, "white")
@@ -30,8 +31,12 @@ def get_valid_moves(board, color):
         check_shifts(board, runsOfThree, valid_moves, color)
     if runsOfTwo:
         check_shifts(board, runsOfTwo, valid_moves, color)
+    for col in drop_order:
+        if valid_row(board, col) != 0:
+            valid_moves.append("D " + str(col + 1))
     return valid_moves
 
+# find what row is empty in a given col
 def valid_row(board, col):
     if board[7][col] == "x":
         return 8
@@ -51,7 +56,8 @@ def valid_row(board, col):
         return 1
     else:
         return 0
-    
+
+# check if cells above a certain col,row is empty
 def is_above_empty(board, row, col):
     is_empty = True
     for i in range(row):
@@ -59,12 +65,14 @@ def is_above_empty(board, row, col):
             is_empty = False
     return is_empty
 
+# print out the playing board
 def print_board(board):
         for row in board:
             for val in row:
                 print(val, end = ",")
             print() 
 
+# drop a piece in the given column
 def drop_piece(board, col, color):
     col = int(col)
     row = valid_row(board, col)
@@ -73,6 +81,7 @@ def drop_piece(board, col, color):
     else:
         board[row - 1][col] = '1'
 
+# reads the move that was given by the AI, player, or server
 def read_move(board, string, color):
     valid_moves = get_valid_moves(board, color)
     if string in valid_moves:    
@@ -89,6 +98,7 @@ def read_move(board, string, color):
     else:
         print("Move is not valid")
 
+# check if there are any winning moves for a given color
 def is_winning_move(board, color):
     winning_runs = []
     if color == "white":
@@ -118,6 +128,7 @@ def is_winning_move(board, color):
 
     return winning_runs
 
+# check horizontal runs of threes for a given color
 def runs_of_three_hor(board, color):
     runs_of_three = []
     if color == "white":
@@ -132,6 +143,7 @@ def runs_of_three_hor(board, color):
                 
     return runs_of_three
 
+# check runs of threes for a given color
 def runs_of_three(board, color):
     runs_of_three = []
     if color == "white":
@@ -161,6 +173,7 @@ def runs_of_three(board, color):
 
     return runs_of_three
 
+# check horizontal runs of twos for a given color
 def runs_of_two_hor(board, color):
     runs_of_two = []
     if color == "white":
@@ -175,6 +188,7 @@ def runs_of_two_hor(board, color):
                 
     return runs_of_two
 
+# check runs of twos for a given color
 def runs_of_two(board, color):
     runs_of_two = []
     if color == "white":
@@ -204,6 +218,7 @@ def runs_of_two(board, color):
 
     return runs_of_two
 
+# check what shifts are possible for a given color
 def check_shifts(board, runs, valid_moves, color):
     for run in runs:
         if run[0][0] != 1:
@@ -226,6 +241,7 @@ def check_shifts(board, runs, valid_moves, color):
                         valid_moves.append("R " + str(run[0][0]) + " " + str(run[0][1]))
     return valid_moves
 
+# fill the current column, when a shift occured
 def fill_column(board, row, col):
     if board[row][col] == 'x':
         cur_row = row
@@ -235,6 +251,7 @@ def fill_column(board, row, col):
         if not is_above_empty(board, row, col):
             fill_column(board, row, col)
 
+# Shift a run left
 def shift_left(board, col, row, main_color):
     col = int(col)
     row = int(row)
@@ -277,6 +294,7 @@ def shift_left(board, col, row, main_color):
                 fill_column(board, bot_row3, col - 4)
     fill_column(board, row, col)
 
+# Shift a run right
 def shift_right(board, col, row, main_color):
     col = int(col)
     row = int(row)
@@ -319,17 +337,31 @@ def shift_right(board, col, row, main_color):
                 fill_column(board, bot_row3, col + 4)
     fill_column(board, row, col)
 
-def score_function(board, color):
-    white = len(runs_of_three(board,"white")) + len(runs_of_two(board,"white"))
-    black = len(runs_of_three(board,"black")) + len(runs_of_two(board,"black"))
-    if color == "white":
-        score = white - black
+# score a non terminal node for minimax
+def score_function(board, color, type):
+    if type == "old":
+        white = len(runs_of_three(board,"white")) + len(runs_of_two(board,"white"))
+        black = len(runs_of_three(board,"black")) + len(runs_of_two(board,"black"))
+        if color == "white":
+            score = white - black
+        else:
+            score = black - white
+        return score
     else:
-        score = black - white
-    return score
+        if color == "white":
+            white3s = len(runs_of_three(board,"white"))
+            white2s = len(runs_of_two(board,"white"))
+            white2shor = len(runs_of_two_hor(board, "white"))
+            score = (3 * white3s) + white2shor + white2s
+        else:
+            black3s = len(runs_of_three(board,"black"))
+            black2s = len(runs_of_two(board,"black"))
+            black2shor = len(runs_of_two_hor(board, "black"))
+            score = (3 * black3s) + black2shor + black2s
+        return score
 
-def minimax(board, cur_depth, max_depth, max_player):
-    global states_visited
+# Minimax algorithm
+def minimax(board, cur_depth, max_depth, max_player, type):
     if is_white:
         color = "white"
         opp_color = "black"
@@ -353,13 +385,13 @@ def minimax(board, cur_depth, max_depth, max_player):
 
     if cur_depth == max_depth:
         if is_white and not max_player:
-            return (None, score_function(board, "white"))
+            return (None, score_function(board, "white", type))
         elif is_white and max_player:
-            return (None, score_function(board, "black"))
+            return (None, score_function(board, "black", type))
         elif not is_white and not max_player:
-            return (None, score_function(board, "black"))
+            return (None, score_function(board, "black", type))
         elif not is_white and max_player:
-            return (None, score_function(board, "white"))
+            return (None, score_function(board, "white", type))
 
     if max_player:
         value = -999999
@@ -368,8 +400,7 @@ def minimax(board, cur_depth, max_depth, max_player):
         for move in validMoves:
             board_copy = board.copy()
             read_move(board_copy, move, color)
-            new_score = minimax(board_copy, cur_depth + 1, max_depth, False)[1]
-            states_visited = states_visited + 1
+            new_score = minimax(board_copy, cur_depth + 1, max_depth, False, type)[1]
             if new_score > value:
                 value = new_score
                 maxMove = move
@@ -381,15 +412,14 @@ def minimax(board, cur_depth, max_depth, max_player):
         for move in validMoves:
             board_copy = board.copy()
             read_move(board_copy, move, opp_color)
-            new_score = minimax(board_copy, cur_depth + 1, max_depth, True)[1]
-            states_visited = states_visited + 1
+            new_score = minimax(board_copy, cur_depth + 1, max_depth, True, type)[1]
             if new_score < value:
                 value = new_score
                 minMove = move
         return (minMove, value)
 
-def minimax_alpha_beta(board, cur_depth, max_depth, max_player, alpha, beta):
-    global states_visited
+# minimax algorithm with alpha-beta pruning
+def minimax_alpha_beta(board, cur_depth, max_depth, max_player, alpha, beta, type):
     if is_white:
         color = "white"
         opp_color = "black"
@@ -413,23 +443,22 @@ def minimax_alpha_beta(board, cur_depth, max_depth, max_player, alpha, beta):
 
     if cur_depth == max_depth:
         if is_white and not max_player:
-            return (None, score_function(board, "white"))
+            return (None, score_function(board, "white", type))
         elif is_white and max_player:
-            return (None, score_function(board, "black"))
+            return (None, score_function(board, "black", type))
         elif not is_white and not max_player:
-            return (None, score_function(board, "black"))
+            return (None, score_function(board, "black", type))
         elif not is_white and max_player:
-            return (None, score_function(board, "white"))
+            return (None, score_function(board, "white", type))
 
     if max_player:
-        value = -999999
+        value = -999998
         validMoves = get_valid_moves(board, color)
         maxMove = validMoves[0]
         for move in validMoves:
             board_copy = board.copy()
             read_move(board_copy, move, color)
-            new_score = minimax_alpha_beta(board_copy, cur_depth + 1, max_depth, False, alpha, beta)[1]
-            states_visited = states_visited + 1
+            new_score = minimax_alpha_beta(board_copy, cur_depth + 1, max_depth, False, alpha, beta, type)[1]
             if new_score > value:
                 value = new_score
                 maxMove = move
@@ -438,14 +467,13 @@ def minimax_alpha_beta(board, cur_depth, max_depth, max_player, alpha, beta):
                 break
         return (maxMove, value)
     else:
-        value = 999999
+        value = 999998
         validMoves = get_valid_moves(board, opp_color)
         minMove = validMoves[0]
         for move in validMoves:
             board_copy = board.copy()
             read_move(board_copy, move, opp_color)
-            new_score = minimax_alpha_beta(board_copy, cur_depth + 1, max_depth, True, alpha, beta)[1]
-            states_visited = states_visited + 1
+            new_score = minimax_alpha_beta(board_copy, cur_depth + 1, max_depth, True, alpha, beta, type)[1]
             if new_score < value:
                 value = new_score
                 minMove = move
@@ -454,94 +482,91 @@ def minimax_alpha_beta(board, cur_depth, max_depth, max_player, alpha, beta):
                 break
         return (minMove, value)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-try:
-    s.connect((TCP_IP, TCP_PORT))
-    print("Connected")
-except Exception as e:
-    print("something wrong")
-s.send((game_id + " " + playing_color).encode())
+# main function
+if connect_to_server:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.connect((TCP_IP, TCP_PORT))
+        print("Connected")
+    except Exception as e:
+        print("something wrong")
+    s.send((game_id + " " + playing_color).encode())
 
-while (game_won == False):
-    if playing_color == "white":
-        minimax_result = minimax_alpha_beta(playing_board, 0, depth, True, -999999, 999999)
-        user_output = minimax_result[0]
-        print(user_output)
-        s.send(user_output.encode())
-        read_move(playing_board, user_output, "white")
-        print_board(playing_board)
-        is_white = False
-        data = s.recv(BUFFER_SIZE).decode()
-        read_move(playing_board, data, "black")
-        is_white = True
-        print_board(playing_board)
-    else:
-        data = s.recv(BUFFER_SIZE).decode()
-        read_move(playing_board, data, "white")
-        print_board(playing_board)
-        is_white = False
-        minimax_result = minimax_alpha_beta(playing_board, 0, depth, True, -999999, 999999)
-        user_output = minimax_result[0]
-        s.send(user_output.encode())
-        read_move(playing_board, user_output, "black")
-        print_board(playing_board)
-        is_white = True
+    while (game_won == False):
+        if playing_color == "white":
+            minimax_result = minimax_alpha_beta(playing_board, 0, depth, True, -999999, 999999, "new")
+            user_output = minimax_result[0]
+            print(user_output)
+            s.send(user_output.encode())
+            read_move(playing_board, user_output, "white")
+            print_board(playing_board)
+            is_white = False
+            data = s.recv(BUFFER_SIZE).decode()
+            read_move(playing_board, data, "black")
+            is_white = True
+            print_board(playing_board)
+        else:
+            data = s.recv(BUFFER_SIZE).decode()
+            read_move(playing_board, data, "white")
+            print_board(playing_board)
+            is_white = False
+            minimax_result = minimax_alpha_beta(playing_board, 0, depth, True, -999999, 999999, "new")
+            user_output = minimax_result[0]
+            s.send(user_output.encode())
+            read_move(playing_board, user_output, "black")
+            print_board(playing_board)
+            is_white = True
     
-    
+    s.close()
+else:
+    while (game_won == False):
+        if playing_color == "white":
+            if is_white:
+                minimax_result = minimax_alpha_beta(playing_board, 0, depth, True, -999998, 999998, "new")
+                user_input = minimax_result[0]
+            else:
+                if AI_vs_AI:
+                    minimax_result = minimax_alpha_beta(playing_board, 0, depth, True, -999998, 999998, "new")
+                    user_input = minimax_result[0]
+                else:
+                    user_input = input("Enter move: ")
+                
+        else:
+            if is_white:
+                if AI_vs_AI:
+                    minimax_result = minimax_alpha_beta(playing_board, 0, depth, True, -999998, 999998, "new")
+                    user_input = minimax_result[0]
+                else:
+                    user_input = input("Enter move: ")
+            else:
+                minimax_result = minimax_alpha_beta(playing_board, 0, depth, True, -999998, 999998, "new")
+                user_input = minimax_result[0]
 
-'''
-while (game_won == False):
-    if playing_color == "white":
-        if
-
-    if is_white:
-        print(get_valid_moves(playing_board, "white"))
-    else:
-        print(get_valid_moves(playing_board, "black"))
-    
-    start = time.time()
-    if playing_color == "white":
+        print(minimax_result[0])
         if is_white:
-            minimax_result = minimax_alpha_beta(playing_board, 0, depth, True, -999999, 999999)
-            user_input = minimax_result[0]
-    else:
-        if not is_white:
-            minimax_result = minimax_alpha_beta(playing_board, 0, depth, True, -999999, 999999)
-            user_input = minimax_result[0]
+            read_move(playing_board, user_input, "white")
+        else:
+            read_move(playing_board, user_input, "black")
+        print_board(playing_board)
+        if is_white:
+            color = "white"
+        else:
+            color = "black"
+        winning_runs = is_winning_move(playing_board, color)
 
-    #minimax_result = minimax(playing_board, 0, depth, True)
-<<<<<<< HEAD
-    if is_white:
-    	minimax_result = minimax_alpha_beta(playing_board, 0, depth, True, -999999, 999999)
-=======
->>>>>>> refs/remotes/origin/main
-    end = time.time()
-    print(minimax_result)
-    print(str(end - start) + "seconds")
-<<<<<<< HEAD
-    user_input = input("Enter move: ")
-    #user_input = minimax_result[0]
-=======
-    #user_input = input("Enter move: ")
-    
->>>>>>> refs/remotes/origin/main
-    print(user_input)
-    if is_white:
-        read_move(playing_board, user_input, "white")
-    else:
-        read_move(playing_board, user_input, "black")
-    print_board(playing_board)
-    if is_white:
-        color = "white"
-    else:
-        color = "black"
-    winning_runs = is_winning_move(playing_board, color)
-    print("States visited: " + str(states_visited))
+        if winning_runs:
+            game_won = True
+            if color == "white" and len(winning_runs) > 0:
+                print("White wins!")
+            elif color == "white" and len(winning_runs) < 0:
+                print("Black wins!")
+            elif color == "black" and len(winning_runs) > 0:
+                print("Black wins!")
+            elif color == "black" and len(winning_runs) < 0:
+                print("White wins!")
+            else:
+                print("Draw!")
+        else:
+            is_white = not is_white
 
-    if winning_runs:
-        print(winning_runs)
-        game_won = True
-    else:
-        is_white = not is_white
-'''
-s.close()
+

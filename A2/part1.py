fe21816dc549453921cb36697db4a6b991a07bc6
jeremy-piperni <jeremypiperni@hotmail.com>
2024-10-12 23:05:49 -0400

    Removed pandas, fixed part1

--- A2/part1.py
+++ A2/part1.py
@@ -1,5 +1,4 @@
 import file_fetcher
-import pandas as pd
 import random
 
 vocab, unigram, bigram, trigram = file_fetcher.clean_files()
@@ -7,76 +6,84 @@ vocab, unigram, bigram, trigram = file_fetcher.clean_files()
 
 def getNextWord(pre_index, index, order):
 	if order == 3:
-		trigram_temp = trigram[trigram["Word1"] == pre_index]
-		trigram_temp = trigram_temp[trigram_temp["Word2"] == index].sort_values(by=["Probability"], ascending=False)
-		print(trigram_temp)
-		if trigram_temp.empty:
-			word, vocabIndex = getNextWord(pre_index, index, 2)
+		trigram_temp0 = []
+		trigram_temp = []
+		for i in range(len(trigram)):
+			if trigram[i][0] == pre_index:
+				trigram_temp0.append(trigram[i])
+		for i in range(len(trigram_temp0)):
+			if trigram_temp0[i][1] == index:
+				trigram_temp.append(trigram_temp0[i])
+		if not trigram_temp:
+			word, vocabIndex, order = getNextWord(pre_index, index, 2)
 		else:
-			"""for ind, row in trigram_temp.iterrows():
-				bigram_prob = bigram[bigram["Word1"] == row["Word1"]]
-				bigram_prob = bigram_prob[bigram_prob["Word2"] == row["Word2"]]
-				prob = bigram_prob.iloc[0,2]
-				trigram_temp.loc[ind,"Probability"] = format(row["Probability"] / prob, 'f')
-			"""
-			sumofp = trigram_temp["Probability"].sum()
+			sumofp = 0
+			for i in range(len(trigram_temp)):
+				sumofp = sumofp + trigram_temp[i][3]
 			prob = random.uniform(0,sumofp)
 			lowbound = 0
-			newWordIndex = 1
+			newWordIndex = 0
 			probSum = 0
-			for ind, row in trigram_temp.iterrows():
-				probSum = probSum + row["Probability"]
+			for i in range(len(trigram_temp)):
+				probSum = probSum + trigram_temp[i][3]
 				if (prob < probSum and prob > lowbound):
 					break
 				else:
 					newWordIndex = newWordIndex + 1
 					lowbound = probSum
-			vocabIndex = trigram_temp.iloc[newWordIndex-1,2]
+			vocabIndex = trigram_temp[newWordIndex][2]
 	if order == 2:
-		bigram_temp = bigram[bigram["Word1"] == index].sort_values(by=["Probability"], ascending=False)
-		print(bigram_temp)
-		sumofp = bigram_temp["Probability"].sum()
+		bigram_temp = []
+		for i in range(len(bigram)):
+			if bigram[i][0] == index:
+				bigram_temp.append(bigram[i])
+		sumofp = 0
+		for i in range(len(bigram_temp)):
+			sumofp = sumofp + bigram_temp[i][2]
 		prob = random.uniform(0,sumofp)
 		lowbound = 0
-		newWordIndex = 1
+		newWordIndex = 0
 		probSum = 0
-		for ind, row in bigram_temp.iterrows():
-			probSum = probSum + row["Probability"]
+		for i in range(len(bigram_temp)):
+			probSum = probSum + bigram_temp[i][2]
 			if (prob < probSum and prob > lowbound):
 				break
 			else:
 				newWordIndex = newWordIndex + 1
 				lowbound = probSum
-		vocabIndex = bigram_temp.iloc[newWordIndex-1,1]
-	
-	print(sumofp)
-	print(prob)
-	print(newWordIndex)
-	return vocab.loc[str(vocabIndex),"Word"], vocabIndex
+		vocabIndex = bigram_temp[newWordIndex][1]
+
+	return vocab[int(vocabIndex) - 1][1], vocabIndex, order
 
 
 sentence = ["<s>"]
+source = []
 pre_index = "0"
 index = "153"
 
-vocab = pd.DataFrame(data=vocab,columns=["Index","Word"])
-vocab = vocab.set_index("Index")
-unigram = pd.DataFrame(data=unigram,columns=["Index","Probability"])
-bigram = pd.DataFrame(data=bigram,columns=["Word1","Word2","Probability"])
-trigram = pd.DataFrame(data=trigram,columns=["Word1","Word2","Word3","Probability"])
-
-newWord, vocabIndex = getNextWord(pre_index, index, 2)
+newWord, vocabIndex, order = getNextWord(pre_index, index, 2)
 sentence.append(newWord)
+source.append(newWord + ": Bigram")
+
 
 while 1:
 	pre_index = index
 	index = vocabIndex
-	newWord, vocabIndex = getNextWord(pre_index, index, 3)
+	newWord, vocabIndex, order = getNextWord(pre_index, index, 3)
 	sentence.append(newWord)
+	if order == 3:
+		source.append(newWord + ": Trigram")
+	elif order == 2:
+		source.append(newWord + ": Bigram")
+	else:
+		source.append(newWord + ": Unigram")
 	if sentence[-1] == "</s>":
 		break
 
 sentence = sentence[1:-1]
-
+print("Generated Sentence:")
 print(*sentence)
+print()
+print("Sources:")
+print(source)
 
